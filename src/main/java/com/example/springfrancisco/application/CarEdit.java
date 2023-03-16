025b1eed0e2dedcd65d6878f156fbb52eb58844b
Angel Espinosa <danielespinosa201517@gmail.com>
2023-03-16 12:21:04 -0500

    TERMINADO CON VALIDACIONES

--- src/main/java/com/example/springfrancisco/application/CarEdit.java
+++ src/main/java/com/example/springfrancisco/application/CarEdit.java
@@ -2,16 +2,25 @@ package com.example.springfrancisco.application;
 
 import com.example.springfrancisco.domain.entities.Carro;
 import com.example.springfrancisco.domain.service.CarEditService;
+import com.example.springfrancisco.domain.service.CarGetService;
 import com.example.springfrancisco.infrastructure.repository.CarRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CarEdit {
     private CarEditService careditservice;
-
+    private final CarGetService carGetService;
     public void editCar(Carro carro){
+        Carro carro1 = carGetService.getCar(carro.getPlaca());
+        if (Objects.isNull(carro1)) {
+            log.info("EL CARRO CON PLACA {} NO ESTA REGISTRADO", carro.getPlaca());
+        }
         careditservice.editCar(carro);
     }
 }
--- src/main/java/com/example/springfrancisco/application/MotoEdit.java
+++ src/main/java/com/example/springfrancisco/application/MotoEdit.java
@@ -1,15 +1,27 @@
 package com.example.springfrancisco.application;
 import com.example.springfrancisco.domain.entities.Moto;
+import com.example.springfrancisco.domain.service.CarGetService;
 import com.example.springfrancisco.domain.service.MotoEditService;
+import com.example.springfrancisco.domain.service.MotoGetService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
 @Service
 @AllArgsConstructor
+@Slf4j
 
 
 public class MotoEdit {
     private MotoEditService motoEditService;
+    private final MotoGetService motoGetService;
     public void editMoto(Moto moto) {
+        Moto carro1 = motoGetService.getMoto(moto.getPlaca());
+        if (Objects.isNull(carro1)) {
+            log.info("LA MOTO CON PLACA {} NO ESTA REGISTRADO", moto.getPlaca());
+        }
         motoEditService.editMoto(moto);
     }
 }
