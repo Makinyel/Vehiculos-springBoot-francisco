3430106f1c7b7408c03f12156ee8a96a68021926
Angel Espinosa <danielespinosa201517@gmail.com>
2023-03-16 17:49:49 -0500

    TERMINADO CON EL RECEIVER

--- src/main/java/com/example/springfrancisco/infrastructure/event/listen/Receiver.java
+++ src/main/java/com/example/springfrancisco/infrastructure/event/listen/Receiver.java
@@ -1,17 +1,29 @@
 package com.example.springfrancisco.infrastructure.event.listen;
 
+import com.example.springfrancisco.application.CarGet;
+import com.example.springfrancisco.domain.entities.Carro;
+import com.example.springfrancisco.domain.service.CarGetService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 
+@AllArgsConstructor
 @Component
 @Slf4j
 public class Receiver {
 
-    public static String RECEIVE_METHOD_NAME = "receiveMessage";
-    public void receiveMessage(byte[] message) {
-        log.info("Receiving message: [{}]", new String(message, StandardCharsets.UTF_8));
-    }
+    private final ObjectMapper objectMapper;
+    private final CarGet carGet;
 
+    public static String RECEIVE_METHOD_NAME = "receiveMessage";
+    public void receiveMessage(byte[] message) throws JsonProcessingException {
+            String mesajeconvertido = new String(message, StandardCharsets.UTF_8);
+            Carro carro = objectMapper.readValue(mesajeconvertido, Carro.class);
+            log.info("Receiving message: [{}]",
+                    objectMapper.writeValueAsString(carGet.getCar(carro.getPlaca())));
+    }
 }
